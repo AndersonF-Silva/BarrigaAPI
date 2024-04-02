b5c3af1314210c61481847c16aef09b8b2dc835f
Anderson Fernando da Silva <anderson_fdasilva@hotmail.com>
2024-04-02 14:49:03 -0300

    Ajuste na impressao da configuracao do Setup de testes.

--- src/main/java/br/com/chronosacademy/Core/BaseTest.java
+++ src/main/java/br/com/chronosacademy/Core/BaseTest.java
@@ -9,7 +9,6 @@ import org.junit.BeforeClass;
 public class BaseTest implements Constantes {
     @BeforeClass
     public static void setup() {
-        System.out.println("O Teste foi Configurado com sucesso!");
         RestAssured.baseURI = APP_BASE_URL;
         RestAssured.port = APP_PORT;
         RestAssured.basePath = APP_BASE_PATH;
@@ -23,5 +22,6 @@ public class BaseTest implements Constantes {
         RestAssured.responseSpecification = resBuilder.build();
 
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        System.out.println("O Teste foi Configurado com sucesso!");
     }
 }
